8017c60053fa1d7a4d1540511040eb5bd03ea5b6
taeseongyun <yts0646@gmail.com>
2019-05-02 21:28:55 +0900

    15 : MODIFY -> Change Started At NameInputActivity

--- app/src/main/java/tech/tsdev/github_management/view/main/activity/NameInputActivity.kt
+++ app/src/main/java/tech/tsdev/github_management/view/main/activity/NameInputActivity.kt
@@ -1,7 +1,9 @@
 package tech.tsdev.github_management.view.main.activity
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_name_input.*
 import tech.tsdev.github_management.R
 
 class NameInputActivity : AppCompatActivity() {
@@ -9,5 +11,9 @@ class NameInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name_input)
+
+        text_input_layout.apply {
+            helperText = "*Require"
+        }
     }
 }
