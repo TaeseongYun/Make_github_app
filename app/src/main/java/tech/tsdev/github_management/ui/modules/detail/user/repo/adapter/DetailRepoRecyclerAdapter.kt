0f0286de996c162bcc48b36ffaf3d8ebc48352b3
taeseongyun <yts0646@gmail.com>
2019-05-12 12:29:19 +0900

    36 : Build -> DetailRepoRecyclerView , ADD -> UserVectorImg

--- app/src/main/java/tech/tsdev/github_management/ui/modules/detail/user/repo/adapter/DetailRepoRecyclerAdapter.kt
+++ app/src/main/java/tech/tsdev/github_management/ui/modules/detail/user/repo/adapter/DetailRepoRecyclerAdapter.kt
@@ -1,2 +1,37 @@
 package tech.tsdev.github_management.ui.modules.detail.user.repo.adapter
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import tech.tsdev.github_management.model.UserRepoList
+import tech.tsdev.github_management.ui.modules.detail.user.repo.adapter.holder.DetailRepoRecyclerHolder
+import tech.tsdev.github_management.ui.modules.detail.user.repo.adapter.model.DetailRepoRecyclerModel
+
+class DetailRepoRecyclerAdapter(val context: Context?) : RecyclerView.Adapter<RecyclerView.ViewHolder>(), DetailRepoRecyclerModel {
+
+    private val userRepoList = mutableListOf<UserRepoList>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): RecyclerView.ViewHolder =
+        DetailRepoRecyclerHolder(context, parent)
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as DetailRepoRecyclerHolder).onBind(userRepoList[position])
+    }
+
+    override fun addItem(items: UserRepoList) {
+        userRepoList.add(items)
+    }
+
+    override fun getItemCount(): Int = userRepoList.size
+
+    override fun getItem(position: Int): UserRepoList = userRepoList[position]
+
+    override fun deleteItemsData() {
+        userRepoList.clear()
+    }
+
+    override fun nofityedItemData() {
+        notifyDataSetChanged()
+    }
+
+}
--- app/src/main/java/tech/tsdev/github_management/ui/modules/detail/user/repo/adapter/holder/DetailRepoRecyclerHolder.kt
+++ app/src/main/java/tech/tsdev/github_management/ui/modules/detail/user/repo/adapter/holder/DetailRepoRecyclerHolder.kt
@@ -1,10 +1,12 @@
 package tech.tsdev.github_management.ui.modules.detail.user.repo.adapter.holder
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.user_repo_detail_items.view.*
 import tech.tsdev.github_management.R
 import tech.tsdev.github_management.model.UserRepoList
 
@@ -15,7 +17,15 @@ class DetailRepoRecyclerHolder(context: Context?, parent: ViewGroup) : RecyclerV
         itemView.onBind(items)
     }
 
+    @SuppressLint("SetTextI18n")
     fun View.onBind(items: UserRepoList) {
-
+        user_repo_avatar.proflieImageLoad(items.owner.avatarUrl)
+        tv_user_repo_name.text = items.name
+        items.description?.let { tv_user_description.text = it } ?: let { tv_user_description.text = "no Description" }
+        tv_user_repo_star.text = items.stargazersCount.toString()
+        tv_user_fork.text = items.forksCount.toString()
+        tv_user_login.text = items.owner.login
+        if(items.fork)
+            tv_fork_repo.visibility = View.VISIBLE
     }
 }
