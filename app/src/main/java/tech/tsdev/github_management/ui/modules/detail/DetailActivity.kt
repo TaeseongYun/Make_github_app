caf0ab7bb79512cb65b9ad80b6ac0eb50d11516e
taeseongyun <yts0646@gmail.com>
2019-06-11 23:13:41 +0900

    74 Rebuild -> DetailActivity Design & Activity Code    ADD -> BottomAppbarMenu

--- app/src/main/java/tech/tsdev/github_management/ui/modules/detail/DetailActivity.kt
+++ app/src/main/java/tech/tsdev/github_management/ui/modules/detail/DetailActivity.kt
@@ -26,10 +26,10 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun addTabLayoutItem() {
-        tab_layout.addTab(tab_layout.newTab().setText(R.string.overview))
-        tab_layout.addTab(tab_layout.newTab().setText(R.string.repository))
-        tab_layout.addTab(tab_layout.newTab().setText(R.string.followers))
-        tab_layout.addTab(tab_layout.newTab().setText(R.string.following))
+//        tab_layout.addTab(tab_layout.newTab().setText(R.string.overview))
+//        tab_layout.addTab(tab_layout.newTab().setText(R.string.repository))
+//        tab_layout.addTab(tab_layout.newTab().setText(R.string.followers))
+//        tab_layout.addTab(tab_layout.newTab().setText(R.string.following))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,39 +37,39 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
 
-        tv_user_name.text = intent.getStringExtra( SearchActivity.USER_NAME )
+//        tv_user_name.text = intent.getStringExtra( SearchActivity.USER_NAME )
 
 
 
         addTabLayoutItem()
 
-        img_close_btn.setOnClickListener {
+        fab_home.setOnClickListener {
             finish()
         }
 
 
 
         // 뷰페이저에서 어뎁터 붙혀주고 addOnPageChangeListener 는 페이지가 변경 될때 Tab도 같이 변하게 묶어주는 리스너
-        view_pager.run {
-            adapter = viewAdapter
-            addOnPageChangeListener(object : TabLayout.TabLayoutOnPageChangeListener(tab_layout){})
-        }
-
-        //탭 레이아웃 클릭 할때 해당하는 포지션의 fragment를 보여줌 (즉 바인딩 해줌)
-        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            override fun onTabReselected(p0: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabUnselected(p0: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                view_pager.currentItem = tab.position
-            }
-
-        })
+//        view_pager.run {
+//            adapter = viewAdapter
+//            addOnPageChangeListener(object : TabLayout.TabLayoutOnPageChangeListener(tab_layout){})
+//        }
+//
+//        //탭 레이아웃 클릭 할때 해당하는 포지션의 fragment를 보여줌 (즉 바인딩 해줌)
+//        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+//            override fun onTabReselected(p0: TabLayout.Tab?) {
+//
+//            }
+//
+//            override fun onTabUnselected(p0: TabLayout.Tab?) {
+//
+//            }
+//
+//            override fun onTabSelected(tab: TabLayout.Tab) {
+//                view_pager.currentItem = tab.position
+//            }
+//
+//        })
     }
 
     inner class DetailViewPagerAdapter(fm: FragmentManager, val context: Context) : FragmentStatePagerAdapter(fm) {
