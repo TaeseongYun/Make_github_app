ad7f78107b1587a6780e639eedb9721207987f8f
taeseongyun <yts0646@gmail.com>
2019-08-01 15:20:07 +0900

    CustomMenu 작성중

--- app/src/main/java/tech/tsdev/github_management/view/main/activity/MainActivity.kt
+++ app/src/main/java/tech/tsdev/github_management/view/main/activity/MainActivity.kt
@@ -1,6 +1,9 @@
 package tech.tsdev.github_management.view.main.activity
 
 import android.content.Intent
+import android.content.res.Resources
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 
 import android.os.Bundle
 
@@ -9,10 +12,19 @@ import android.transition.Fade
 import android.transition.Slide
 import android.transition.TransitionInflater
 import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.load.engine.Resource
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.yalantis.contextmenu.lib.ContextMenuDialogFragment
+import com.yalantis.contextmenu.lib.MenuGravity
+import com.yalantis.contextmenu.lib.MenuObject
+import com.yalantis.contextmenu.lib.MenuParams
 import kotlinx.android.synthetic.main.main_bottom_navigation.*
 import kotlinx.android.synthetic.main.toolbar_main.*
+import org.jetbrains.anko.toast
 import tech.tsdev.github_management.R
 import tech.tsdev.github_management.util.replace
 import tech.tsdev.github_management.view.main.myfragment.MyFragment
@@ -22,11 +34,11 @@ import tech.tsdev.github_management.view.main.userlistfragment.GithubFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val githubFragment : GithubFragment by lazy {
+    private val githubFragment: GithubFragment by lazy {
         GithubFragment()
     }
 
-    private val myGithubFragment : MyFragment by lazy {
+    private val myGithubFragment: MyFragment by lazy {
         MyFragment().apply {
             arguments = Bundle().apply {
                 putString("userName", intent.getStringExtra("inputUserName"))
@@ -43,9 +55,8 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.navigation_users -> {
                 replace(R.id.frame_layout, githubFragment)
                 return@OnNavigationItemSelectedListener true
@@ -63,10 +74,29 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.change_user_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            when (it.itemId) {
+                R.id.change_user -> {
+
+                }
+                else -> {
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(custom_toolbar)
 
         replace(R.id.frame_layout, githubFragment)
 
@@ -78,6 +108,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, SearchActivity::class.java))
         }
 
-        
+        val bitmapDrawable = BitmapDrawable(
+            resources,
+            BitmapFactory.decodeResource(resources, R.drawable.ic_keyboard_arrow_down_black_24dp)
+        )
+        val close = MenuObject().apply {
+            drawable = bitmapDrawable
+            menuTextAppearanceStyle = R.style.TextViewStyle
+        }
+        val menuObject = mutableListOf<MenuObject>().apply {
+            add(close)
+        }
+        ContextMenuDialogFragment.newInstance(
+            MenuParams(
+                resources.getDimension(R.dimen.text_large).toInt(),
+                menuObjects = menuObject,
+                isClosableOutside = false,
+                gravity = MenuGravity.END
+            )
+        )
     }
 }
